9a622879db0c9da43eb21afc7c5d14f8105da52d
sagOcean <newlaptop310@gmail.com>
2026-02-26 13:23:48 +0545

    reversing an array using pointer

---
+++ pointer-questions-solve.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    /*reversing an array  using pointer
+
+    int num[]={45,36,57,23,56,76};
+
+    int len = sizeof(num)/sizeof(int);
+
+    int* start = &num[0];
+    int* end = &num[len-1];
+
+    while(start<=end){
+        printf("%d\t",*end);
+        end--;
+    }
+    
+    */
+
+    
+    
+    return 0;
+}
